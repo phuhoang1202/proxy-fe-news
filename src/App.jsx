40e81fa15f916f8f34519456bac455e1e0730bdd
phuhoang1202 <phuhoang1202@onlykcs.com>
2025-03-01 21:00:24 +0700

    phuht

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import HomePage from '@pages/user/homePage/HomePage.jsx'
+import HomePage from '@pages/user/homepage/HomePage.jsx'
 import LayoutUser from '@pages/user/layout/LayoutUser.jsx'
 import DetailProduct from '@pages/user/detailProduct/DetailProduct.jsx'
 import NotFoundPage from '@components/users/404/NotFoundPage.jsx'
